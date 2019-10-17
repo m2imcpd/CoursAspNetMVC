3fca9d6d26f9fa047ac80cbb3665fb5ba070f980
ihab <m2imcdp@gmail.com>
2019-10-17 15:42:44 +0200

    Cours ASP.NET V1

---
+++ CoursAspNet/Controllers/ClientController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoursAspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoursAspNet.Controllers
+{
+    public class ClientController : Controller
+    {
+        public string GetClient()
+        {
+            return "abadi";
+        }
+
+        public IActionResult GetAllClients()
+        {
+            List<object> listeClients = new List<object>();
+            listeClients.Add(new { Nom = "toto", Prenom = "tata" });
+            listeClients.Add(new { Nom = "titi", Prenom = "minet" });
+
+            return Json(listeClients);
+        }
+
+        public IActionResult GetContentClient()
+        {
+            Response.ContentType = "text/html";
+            return Content("<html><head></head><body><h1>Bonjour tout le monde</h1></body></html>");
+        }
+
+        public IActionResult GetViewClient()
+        {
+            //return view même nom que l'action
+            //return View()
+            //return view qui s'appelle Index
+            //return View("Index");
+            //return view avec le chemin de la view
+
+            ViewData["nom"] = "abadi";
+            ViewData["prenom"] = "ihab";
+
+            List<dynamic> listePersonne = new List<dynamic>
+            {
+                new {Nom = "toto", Prenom="tata"},
+                new {Nom = "titi", Prenom="minet"},
+            };
+
+            ViewData["liste"] = listePersonne;
+
+            ViewBag.Rue = "Paris";
+            ViewBag.Ville = "Tourcoing";
+
+            return View("~/Views/Client/Index.cshtml");
+        }
+
+        public IActionResult DetailPersonne()
+        {
+            PersonneModel personne = new PersonneModel() { Nom = "abadi", Prenom = "ihab" };
+
+            return View(personne);
+        }
+
+        public IActionResult ListePersonnes()
+        {
+            List<PersonneModel> personnes = new List<PersonneModel>();
+            personnes.Add(new PersonneModel { Nom = "tata", Prenom = "titi" });
+            personnes.Add(new PersonneModel { Nom = "toto", Prenom = "minet" });
+
+            return View(personnes);
+        }
+    }
+}
---
+++ CoursAspNet/Models/PersonneModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursAspNet.Models
+{
+    public class PersonneModel
+    {
+        private string nom;
+        private string prenom;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+    }
+}
