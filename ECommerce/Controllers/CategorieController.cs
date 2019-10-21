fc899e1f2e1891e7b51660dc236abd109fa5b5af
ihab <m2imcdp@gmail.com>
2019-10-21 16:10:11 +0200

    Correction Ecommerce Partie 2

--- ECommerce/Controllers/CategorieController.cs
+++ ECommerce/Controllers/CategorieController.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Models;
+using ECommerce.Tools;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Controllers
 {
     public class CategorieController : Controller
     {
+        private DataDbContext data;
+        public CategorieController(DataDbContext d)
+        {
+            data = d;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult FormCategorie()
+        {
+            return View();
+        }
+
+        public IActionResult AddCategory([Bind("Titre")] Categorie categorie, IFormFile image)
+        {
+            if(image != null)
+            {
+                string imageFile = Path.Combine("wwwroot", "images", categorie.Titre + "-" + image.FileName);
+                string urlImage ="~/images/"+categorie.Titre + "-" + image.FileName;
+                var stream = System.IO.File.Create(imageFile);
+                image.CopyTo(stream);
+                Image img = new Image { UrlImage = urlImage };
+                data.Images.Add(img);
+                data.SaveChanges();
+                categorie.ImageCategorie = img;
+            }
+            data.Categories.Add(categorie);
+            data.SaveChanges();
+            return RedirectToAction("Index", new { controller = "Product" });
         }
     }
 }
--- ECommerce/Controllers/ProductController.cs
+++ ECommerce/Controllers/ProductController.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Models;
+using ECommerce.Tools;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ECommerce.Controllers
 {
     public class ProductController : Controller
     {
+        private DataDbContext data;
+
+        public ProductController(DataDbContext _d)
+        {
+            data = _d;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult FormsProduct()
+        {
+            ViewBag.Categories = new List<SelectListItem>();
+            data.Categories.Cast<Categorie>().ToList().ForEach(c =>
+            {
+                SelectListItem s = new SelectListItem(c.Titre, c.Id.ToString());
+                ViewBag.Categories.Add(s);
+            });
             return View();
         }
     }
