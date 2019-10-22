a4f180da7a00380fcae7379245a8ce746d8cc869
ihab <m2imcdp@gmail.com>
2019-10-22 09:52:51 +0200

    correction ajouter un produit

--- ECommerce/Controllers/ProductController.cs
+++ ECommerce/Controllers/ProductController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ECommerce.Models;
 using ECommerce.Tools;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -31,6 +33,28 @@ namespace ECommerce.Controllers
                 ViewBag.Categories.Add(s);
             });
             return View();
+        }
+
+        public IActionResult addProduct([Bind("Title, Description, Price")] Product produit, List<IFormFile> photos, List<int> Categories)
+        {
+            foreach(IFormFile image in photos)
+            {
+                string imageFile = Path.Combine("wwwroot", "images", produit.Title + "-" + image.FileName);
+                string urlImage = "~/images/" + produit.Title + "-" + image.FileName;
+                var stream = System.IO.File.Create(imageFile);
+                image.CopyTo(stream);
+                Image img = new Image { UrlImage = urlImage };
+                data.Images.Add(img);
+                data.SaveChanges();
+                produit.Images.Add(new ImageProduit { Product = produit, Image = img});
+            }
+            foreach(int c in Categories)
+            {
+                produit.Categories.Add(new ProductCategorie { Categorie = data.Categories.Find(c), Product = produit });
+            }
+            data.Products.Add(produit);
+            data.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
