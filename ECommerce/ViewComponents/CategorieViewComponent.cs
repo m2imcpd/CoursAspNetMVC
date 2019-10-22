9d86dc58cfb3e6775f7e4b5dfc29f37640bb8457
ihab <m2imcdp@gmail.com>
2019-10-22 11:13:57 +0200

    Utilisation viewcomponent pour afficher les categories

---
+++ ECommerce/ViewComponents/CategorieViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Models;
+using ECommerce.Tools;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.ViewComponents
+{
+    [ViewComponent(Name ="Categorie")]
+    public class CategorieViewComponent : ViewComponent
+    {
+        private DataDbContext data;
+
+        public CategorieViewComponent(DataDbContext _data)
+        {
+            data = _data;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Categorie> liste = await GetAllCategoriesAsync();
+            return View("Categorie",liste);
+        }
+
+        public Task<List<Categorie>> GetAllCategoriesAsync()
+        {
+            return Task<List<Categorie>>.Run(()=> data.Categories.ToList());
+        }
+
+    }
+}
